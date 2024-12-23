87dc47a7586577d0101a52476b9bf341c20b1918
Maks Zlatkin <maksim.zlatkin@gmail.com>
2024-12-23 17:40:16 -0600

    mongo repository fix

--- location-service/src/main/java/com/stuff/location_service/mapper/HouseholdMapper.java
+++ location-service/src/main/java/com/stuff/location_service/mapper/HouseholdMapper.java
@@ -5,6 +5,7 @@ import com.stuff.location_service.dto.HouseholdDto;
 import com.stuff.location_service.dto.UserDto;
 import com.stuff.location_service.model.Household;
 import com.stuff.location_service.model.User;
+import org.bson.Document;
 
 public class HouseholdMapper {
     public HouseholdDto toHouseholdDto(Household household){
@@ -27,6 +28,23 @@ public class HouseholdMapper {
                 .id(houseHoldId)
                 .owner(householdCreateRequest.getOwner())
                 .sqft(householdCreateRequest.getSqft()).build();
+    }
+
+    public Document toDocument(Household household){
+        Document document = new Document();
+        document.append("id", household.getId());
+        document.append("owner", household.getOwner());
+        document.append("sqft", household.getSqft());
+        document.append("members", household.getMembers());
+        return document;
+    }
+
+    public Household toHousehold(Document document) {
+        return Household.builder()
+                .id(document.getString("id"))
+                .sqft(document.getInteger("sqft"))
+                .members(document.getList("members", User.class))
+                .owner(document.getString("owner")).build();
     }
 
     public User MapToUser(UserDto userDTO){
